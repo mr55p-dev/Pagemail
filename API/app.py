79f13078c9db0463f16f582d92c44d65ca4af522
Ellis <ellislunnon@gmail.com>
2021-01-30 00:03:58 +0000

    Added CORS middleware.

--- API/app.py
+++ API/app.py
@@ -3,6 +3,7 @@ import logging
 import os
 import sys
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 
@@ -22,6 +23,15 @@ from API.routes.users import router as users_router
 
 # Define app and include routers and connection events.
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(users_router)
 app.include_router(pages_router)
 
