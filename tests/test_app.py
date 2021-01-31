ee9ab6c29feff6fc8227476f9b5c15040de76d45
Ellis <ellislunnon@gmail.com>
2021-01-31 22:03:35 +0000

    Added testing module.

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from API.app import app
+
+client = TestClient(app)
+
+def test_main_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.text == '"THE APP IS WORKING?"'
