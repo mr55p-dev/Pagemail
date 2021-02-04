4420ac7212d5a51bb9dd25b867cdbbca8b1817d5
Ellis <ellislunnon@gmail.com>
2021-02-04 20:17:17 +0000

    Added user sign up tests and a basic testing framework.

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,9 +1,22 @@
+from API.db.connection import get_db
 from fastapi.testclient import TestClient
 from API.app import app
+from tests.mock_database import override_get_db
+from tests.data_gen import new_user
+
+import pytest_depends
+import pytest
 
 client = TestClient(app)
+app.dependency_overrides[get_db] = override_get_db
+mock_user = {"username": "mock@username.com", "password": "mock_password"}
 
+@pytest.mark.depends(on=['register'])
 def test_save():
-    pass
+    response = client.post(
+        "/page/save",
+
+    )
+
 def test_mypages():
     pass
