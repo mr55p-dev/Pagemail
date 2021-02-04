188bb6b1e79d17e9c9243fa6ec2a97211bab1174
Ellis <ellislunnon@gmail.com>
2021-02-04 12:58:04 +0000

    Test structure set up

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_main_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.text == '"THE APP IS WORKING?"'
+    assert response.text == '"Hello World."'
