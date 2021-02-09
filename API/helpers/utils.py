9c507baf11ac03a7ff422efb64420db3833e895a
Ellis <ellislunnon@gmail.com>
2021-02-09 19:25:46 +0000

    Began adding a fetch metadata fucntion

--- API/helpers/utils.py
+++ API/helpers/utils.py
@@ -1,6 +1,9 @@
+from uuid import UUID
 from API.helpers.models import Page
 from fastapi import Form, HTTPException
 from pydantic.error_wrappers import ValidationError
+import requests
+from bs4 import BeautifulSoup as BS
 
 
 async def unwrap_submitted_page(url: str = Form("")):
@@ -12,3 +15,15 @@ async def unwrap_submitted_page(url: str = Form("")):
         detail="Malformed request.",
     )
     return page
+
+async def fetch_metadata(id: UUID, url: str):
+    resp = requests.get(url)
+    # Validate response
+
+    # Get page title
+    soup = BS(resp.text)
+    title = soup.title.text
+    
+    # 
+
+
--- API/routes/pages.py
+++ API/routes/pages.py
@@ -1,11 +1,11 @@
 from typing import List
 from uuid import uuid4
 from databases.core import Database
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from fastapi.param_functions import Depends
 from API.db.connection import database, get_db, pages, users
 from API.helpers.models import Page, PageOut, UserIn, UserOut
-from API.helpers.utils import unwrap_submitted_page
+from API.helpers.utils import unwrap_submitted_page, fetch_metadata
 from API.helpers.verification import get_current_active_user
 from sqlalchemy.sql import select
 from datetime import datetime
@@ -17,9 +17,11 @@ router = APIRouter(
 
 @router.post('/save', response_model=Page)
 async def save_new_page(
+    background: BackgroundTasks,
     new_page: Page = Depends(unwrap_submitted_page),
     current_user: UserIn = Depends(get_current_active_user),
-    database: Database = Depends(get_db)):
+    database: Database = Depends(get_db)
+    ):
     
     new_page.id = uuid4()
     new_page.user_id = current_user.id
@@ -27,6 +29,8 @@ async def save_new_page(
 
     query = pages.insert().values(**new_page.dict())
     await database.execute(query=query)
+    # background.add_task(fetch_metadata, id=new_page.id, url=new_page.page_url)
+
     return new_page
 
 @router.get('/mypages', response_model=List[PageOut])
