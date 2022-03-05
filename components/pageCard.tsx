e9dc996a6cc05b88b5d9eb895830515b1cb9a99b
Ellis Lunnon <ellislunnon@gmail.com>
2022-03-05 09:06:13 +0000

    Small tweaks to the responsiveness

--- components/pageCard.tsx
+++ components/pageCard.tsx
@@ -18,10 +18,10 @@ export default function PageCard({ data, deleteCallback }) {
     return (
         <div className="text-secondary dark:text-secondary-dark bg-primary dark:bg-primary-dark
                         grid grid-rows-6 grid-cols-1 sm:grid-rows-1 sm:grid-cols-12 items-stretch
-                        odd:border-y-2 last:border-b-2 border-secondary dark:border-secondary-dark p-2 md:py-4 group">
+                        odd:border-y-2 last:border-b-2 border-secondary dark:border-secondary-dark p-2 md:py-4">
             <a href={url.toString()} target="_blank" rel="noreferrer"
                             className="row-span-4 sm:row-span-2 sm:col-span-10 sm:p-2 lg:col-span-11
-                            whitespace-nowrap overflow-hidden sm:mr-2 hover:cursor-pointer">
+                            whitespace-nowrap overflow-hidden sm:mr-2 hover:cursor-pointer group">
                 <h3 className="transition-all font-semibold group-hover:underline underline-offset-2 text-xl
                     group-hover:text-tertiary group-hover:font-bold">{title}</h3>
                 <p className="mb-2">{(desc.length > 150) ? desc.substr(0, 150-3) + '...' : desc}</p>
@@ -29,7 +29,7 @@ export default function PageCard({ data, deleteCallback }) {
             </a>
             <div className="row-span-2 sm:col-span-2 lg:col-span-1 flex items-center justify-around
                         border-tertiary border-2 text-semibold text-md
-                        group-hover:bg-tertiary group-hover:text-primary group-hover:dark:text-primary-dark
+                        hover:bg-tertiary hover:text-primary hover:dark:text-primary-dark
                         text-secondary dark:text-secondary-dark transition-all">
                 <button className="w-full h-full" onClick={() => {deleteCallback(data.id)}}>Remove</button>
             </div>
