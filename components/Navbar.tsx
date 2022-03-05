95884f0e94077da2f004895de2e77fa96176cce8
Ellis Lunnon <ellislunnon@gmail.com>
2022-03-05 09:19:58 +0000

    Navbar restyle for mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,45 +15,50 @@ export default function Navbar() {
     </svg>)
 
     // Components which are conditional on being signed in
-    const SignOut = user && (
-        <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
-            <button onClick={() => signOut(auth)}>
-                <a>Sign Out</a>
-            </button>
-        </div>)
-
     const signedInDisplay = user ? (
         <>
+        <div className="nav-border" />
         <Link href="/account">
             <div className="btn-shape btn-colour mx-1 my-2 px-3 py-2 hover:cursor-pointer">
                 <a className="inline">{ user.displayName }</a>
-                <img className="rounded inline w-8 h-8 ml-2" src={photoURL} />
+                <img className="rounded-full object-cover inline w-8 h-8 ml-2" src={photoURL} />
             </div>
         </Link>
-        <hr className="border-secondary dark:border-secondary-dark"/>
+        <div className="nav-border" />
         <Link passHref href="/upload">
             <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
                 <a className="nav-link">Upload</a>
             </div>
         </Link>
-        <hr className="border-secondary dark:border-secondary-dark"/>
+        <div className="nav-border" />
         <Link passHref href="/pages">
             <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
                 <a className="nav-link">My Pages</a>
             </div>
         </Link>
+        <div className="nav-border" />
+        <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
+            <button onClick={() => signOut(auth)}>
+                <a>Sign Out</a>
+            </button>
+        </div>
+        <div className="nav-border" />
         </>
     ) : (
+        <>
+        <div className="nav-border" />
         <Link passHref href="/enter">
             <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
                 <a>Sign in</a>
             </div>
         </Link>
+        <div className="nav-border" />
+        </>
     )
 
     return(
         <nav className="md:flex md:justify-between md:items-center text-secondary dark:text-secondary-dark text-center
-                        max-w-screen-xl mx-auto border-b-2 border-secondary md:border-0">
+                        max-w-screen-xl mx-auto">
             <div className="flex justify-between items-center px-3 py-6">
                 <div className="text-3xl">
                     <Link passHref href="/">
@@ -69,23 +74,9 @@ export default function Navbar() {
                 </div>
             </div>
             <div className={mobileShow ? "block" : "hidden md:block"}>
-                <div className="pb-1 border-t-2 border-secondary dark:border-secondary-dark
-                                transition-all md:flex md:items-center md:border-0 md:py-0">
+                <div className="pb-1
+                                transition-all md:flex md:items-center md:py-0">
                     { signedInDisplay }
-                    {/* <hr className="border-secondary"/> */}
-                    {/* <Link passHref href="/about">
-                        <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
-                            <a>About</a>
-                        </div>
-                    </Link>
-                    <hr className="border-secondary"/>
-                    <Link passHref href="/contact">
-                        <div className="btn-shape btn-colour mx-1 my-2 p-3 hover:cursor-pointer">
-                            <a>Contact</a>
-                        </div>
-                    </Link> */}
-                    { user && <hr className="border-secondary dark:border-secondary-dark"/> }
-                    { SignOut }
                 </div>
             </div>
         </nav>
