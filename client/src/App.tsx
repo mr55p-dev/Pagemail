074813a290c3d5bba1bc141f9e27e76280e0dd62
Ellis Lunnon <ellislunnon@gmail.com>
2023-06-04 16:33:30 +0100

    Added UI for sign in/up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,109 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "almond.css/dist/almond.min.css";
+import "./App.css";
+import React from "react";
+import { pb } from "./lib/pocketbase";
+import { Record, RecordAuthResponse } from "pocketbase";
+
+enum DataState {
+  UNKNOWN,
+  SUCCESS,
+  FAILED,
+  PENDING,
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setpassword] = React.useState<string>("");
+  const [errMsg, setErrMsg] = React.useState<string>("");
+  const [authState, setAuthState] = React.useState<DataState>(
+    DataState.UNKNOWN
+  );
+
+  const handleEmailChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+  };
+  const handlepasswordChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setpassword(e.currentTarget.value);
+  };
+
+  const handleSignin = () => {
+    setAuthState(DataState.PENDING);
+    pb.collection("users")
+      .authWithPassword(email, password)
+      .then(() => setAuthState(DataState.SUCCESS))
+      .catch(() => setAuthState(DataState.FAILED));
+  };
+
+  const handleSignup = () => {
+    setAuthState(DataState.PENDING);
+    // example create data
+    const data = {
+      // username: "test_username",
+      email: email,
+      emailVisibility: true,
+      password: password,
+      passwordConfirm: password,
+      // name: "test",
+    };
+    pb.collection("users")
+      .create(data)
+      .then(() => setAuthState(DataState.SUCCESS))
+      .catch((err) => {
+        setAuthState(DataState.FAILED);
+        setErrMsg(err.data.message);
+      });
+  };
+
+  const handleSignout = () => {
+    pb.authStore.clear();
+    setAuthState(DataState.UNKNOWN);
+  };
 
   return (
     <>
+      <h1>Pagemail</h1>
+      {authState !== DataState.SUCCESS ? (
+        <>
+          <div>
+            <h3>Login</h3>
+            <input
+              type="email"
+              onChange={handleEmailChange}
+              value={email}
+              id="email-field"
+            />
+            <label htmlFor="email-field">Email</label>
+            <input
+              type="password"
+              onChange={handlepasswordChange}
+              value={password}
+              id="password-field"
+            />
+            <label htmlFor="password-field">password</label>
+            <button onClick={handleSignin}>Sign in</button>
+            <button onClick={handleSignup}>Sign up</button>
+          </div>
+        </>
+      ) : undefined}
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {authState === DataState.SUCCESS ? (
+          <>
+            <h3>Welcome, {pb.authStore.model?.id}</h3>
+            <button onClick={handleSignout}>Sign out</button>
+          </>
+        ) : authState === DataState.PENDING ? (
+          <h3>Loading...</h3>
+        ) : authState === DataState.FAILED ? (
+          <>
+            <h3>Failed to login</h3>
+            <p>{errMsg}</p>
+          </>
+        ) : authState === DataState.UNKNOWN ? (
+          <h3>Please sign in or sign up!</h3>
+        ) : undefined}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/lib/pocketbase.ts
@@ -0,0 +1,4 @@
+import PocketBase from "pocketbase";
+
+export const pb = new PocketBase('http://127.0.0.1:8090')
+
