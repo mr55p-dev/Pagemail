3e207a05aee087fe5d0610e9f5711ba9feb39f2d
Ellis Lunnon <ellislunnon@gmail.com>
2023-07-25 19:51:14 +0100

    Fixed lint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,6 @@ import AuthPage from "./routes/auth.tsx";
 import PagesPage from "./routes/pages.tsx";
 import {
   Protected,
-  NotProtected,
   NotVerified,
 } from "./components/Protected/Protected.component.tsx";
 import { Index } from "./routes/index.tsx";
