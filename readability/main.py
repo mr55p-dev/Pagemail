223130da8006d347afded3d79cf8cfd8e9980ae0
Ellis Lunnon <ellislunnon@gmail.com>
2023-07-28 00:56:26 +0100

    Checkpoint again, things are working

--- readability/main.py
+++ readability/main.py
@@ -1,61 +1,96 @@
-from functools import reduce
 import re
 import sys
-from pprint import pprint
 import json
-from typing import Self, TextIO
 import boto3
-# import bs4
-import logging
 from html.parser import HTMLParser
-from contextlib import closing
+import logging
 
 class Tag():
-    def __init__(self, tagname: str, isopen: bool) -> None:
+    OPEN = 1
+    CLOSE = 2
+    OPENCLOSE = 3
+    def __init__(self, tagname: str, isopen: int) -> None:
         self.name = tagname
         self.isopen = isopen
 
     def __repr__(self) -> str:
-        if self.isopen:
-            return f"<{self.name}>"
-        else:
-            return f"</{self.name}>"
+        match self.isopen:
+            case Tag.CLOSE:
+                return f"</{self.name}>"
+            case Tag.OPENCLOSE:
+                return f"<{self.name}/>"
+            case Tag.OPEN:
+                return f"<{self.name}>"
+            case _:
+                return f"<{self.name}>"
 
 
 class Parser(HTMLParser):
-    IGNORE = ["div", "main", "article", "a", "header", ""]
-    KEEP = ["section", "p", "article", "h2"]
-    REMAP = {
-        "article": "speak",
-        "strong": "emphasis",
-        "em": "emphasis",
+    KEEP = ["p"]
+
+    REMAP_OPEN = {
+        "section": Tag("break", Tag.OPENCLOSE),
+        "article": Tag("speak", Tag.OPEN),
+        "strong": Tag("emphasis", Tag.OPEN),
+        "em": Tag("emphasis", Tag.OPEN),
+        "h1": Tag("p", Tag.OPEN),
+        "h2": Tag("p", Tag.OPEN),
+        "h3": Tag("p", Tag.OPEN),
+        "h4": Tag("p", Tag.OPEN),
+        "h5": Tag("p", Tag.OPEN),
+        "h6": Tag("p", Tag.OPEN),
+    }
+    
+    REMAP_CLOSE = {
+        "article": Tag("speak", Tag.CLOSE),
+        "strong": Tag("emphasis", Tag.CLOSE),
+        "em": Tag("emphasis", Tag.CLOSE),
+        "h1": Tag("p", Tag.CLOSE),
+        "h2": Tag("p", Tag.CLOSE),
+        "h3": Tag("p", Tag.CLOSE),
+        "h4": Tag("p", Tag.CLOSE),
+        "h5": Tag("p", Tag.CLOSE),
+        "h6": Tag("p", Tag.CLOSE),
     }
 
     def __init__(self, *, convert_charrefs: bool = True) -> None:
         super().__init__(convert_charrefs=convert_charrefs)
-        self.output_stream = []
+        self._output_stream = []
         self.tagstack = []
 
-    def use_tag(self, tag):
-        return tag not in Parser.IGNORE
-
-    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
+    def handle_starttag(self, tag: str, _: list[tuple[str, str | None]]) -> None:
         if tag in Parser.KEEP:
-            self.output_stream.append(Tag(tag, True))
-            self.tagstack.append(True)
+            self._output_stream.append(Tag(tag, Tag.OPEN))
+        elif tag in Parser.REMAP_OPEN:
+            replacement = Parser.REMAP_OPEN[tag]
+            self._output_stream.append(replacement)
+            if replacement.isopen == Tag.OPENCLOSE:
+                self.tagstack.append(False)
+                return 
         else:
             self.tagstack.append(False)
+            return
+        self.tagstack.append(True)
 
     def handle_endtag(self, tag: str) -> None:
-        if self.tagstack.pop():
-            self.output_stream.append(Tag(tag, False))
+        if not self.tagstack.pop():
+            return
+
+        if tag in Parser.REMAP_CLOSE:
+            self._output_stream.append(Parser.REMAP_CLOSE[tag])
+        else:
+            self._output_stream.append(Tag(tag, Tag.CLOSE))
 
     def handle_data(self, data: str) -> None:
         processed_data = preprocess_html_content(data)
-        if len(self.output_stream) and isinstance(self.output_stream[-1], str):
-            self.output_stream[-1] += processed_data
+        if len(self._output_stream) and isinstance(self._output_stream[-1], str):
+            self._output_stream[-1] += processed_data
         elif processed_data:
-            self.output_stream.append(processed_data)
+            self._output_stream.append(processed_data)
+
+    @property
+    def output_stream(self):
+        return "".join(str(i) for i in self._output_stream)
 
 def replace(start: str, init: list[str], repl: list[str]) -> str:
     n = start
@@ -63,41 +98,50 @@ def replace(start: str, init: list[str], repl: list[str]) -> str:
         n = n.replace(l, r)
     return n
 
-
 def preprocess_html_content(content: str) -> str:
     csub = replace(content, ["\"", r"&", r"'", r"<", r">"], ["&quot;", "&amp;", "&apos;", "&lt;", "&gt;"])
-    res = re.sub(r'^\s+$', r'', csub)
+    res = re.sub(r'^\s+$', r' ', csub)
+    res = re.sub(r'\s+', ' ', res)
     return res
 
-def main(text = None):
-    # session = boto3.Session()
-    # client = session.client("polly")
+def main():
+    session = boto3.Session()
+    client = session.client("polly")
 
     # Load the data 
-    if not text:
-        text = sys.stdin.read()
-        data = json.loads(text)
-        with open("bootstrap_py", "w") as f:
-            f.write(json.dumps(data))
+    text = sys.stdin.read()
+    data = json.loads(text)
 
     # Parse the json 
     data = json.loads(text)
-    # with open("test.html", "w") as f:
-    #     f.write(data["content"])
     
     parser = Parser()
     parser.feed(data["content"])
-    print("\n".join(str(i) for i in parser.output_stream))
+    inp = parser.output_stream
 
-    # res = client.synthesize_speech(Text="Hello, world!", OutputFormat="mp3", VoiceId="Joanna")
-    # with closing(res["AudioStream"]) as stream, open("out.mp3", "wb") as f:
-    #     f.write(stream.read())
+    try:
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/polly/client/start_speech_synthesis_task.html#
+        response = client.start_speech_synthesis_task(
+            Engine='standard',
+            LanguageCode='en-GB',
+            OutputFormat='mp3',
+            OutputS3BucketName='pagemail-speechsynthesis',
+            OutputS3KeyPrefix='test2',
+            Text=inp,
+            TextType='ssml',
+            VoiceId='Amy',
+        )
+        sys.stdout.write(json.dumps(response, default=str))
+        sys.stdout.flush()
+    except Exception as e:
+        logging.exception(e)
+        logging.fatal("Failed to create new speech synthesis task")
+        sys.exit(1)
+
+    logging.info("Finished job")
+
 
     
 if __name__ == "__main__":
-    data = None
-    # if len(sys.argv) > 1:
-    #     with open("bootstrap_py", "r") as f:
-    #         data = f.read()
-
-    main(data)
+    logging.basicConfig(filename="parser.log")
+    main()
