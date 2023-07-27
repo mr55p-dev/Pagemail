a2394d370b66a1e10a6af7ad511d169a8c2ffdf4
Ellis Lunnon <ellislunnon@gmail.com>
2023-07-27 23:56:49 +0100

    checkpoint

--- readability/main.py
+++ readability/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import re
 import sys
 from pprint import pprint
 import json
@@ -8,80 +10,68 @@ import logging
 from html.parser import HTMLParser
 from contextlib import closing
 
-class Node:
-    def __init__(self, tag: str):
-        self.tag = tag
-        self.children: list[Self | str] = []
+class Tag():
+    def __init__(self, tagname: str, isopen: bool) -> None:
+        self.name = tagname
+        self.isopen = isopen
 
-    def isTextNode(self) -> bool:
-        return len(self.children) == 1 and isinstance(self.children[0], str)
+    def __repr__(self) -> str:
+        if self.isopen:
+            return f"<{self.name}>"
+        else:
+            return f"</{self.name}>"
+
 
 class Parser(HTMLParser):
     IGNORE = ["div", "main", "article", "a", "header", ""]
-    # IGNORE = []
+    KEEP = ["section", "p", "article", "h2"]
+    REMAP = {
+        "article": "speak",
+        "strong": "emphasis",
+        "em": "emphasis",
+    }
 
     def __init__(self, *, convert_charrefs: bool = True) -> None:
-        self.root = Node("root")
-        self.current_tag = [self.root]
         super().__init__(convert_charrefs=convert_charrefs)
+        self.output_stream = []
+        self.tagstack = []
 
     def use_tag(self, tag):
         return tag not in Parser.IGNORE
 
     def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]) -> None:
-        if not self.use_tag(tag):
-            return
-
-        new = Node(tag)
-        self.current_tag[-1].children.append(new)
-        self.current_tag.append(new)
-        return
+        if tag in Parser.KEEP:
+            self.output_stream.append(Tag(tag, True))
+            self.tagstack.append(True)
+        else:
+            self.tagstack.append(False)
 
     def handle_endtag(self, tag: str) -> None:
-        if not self.use_tag(tag):
-            return
-
-        self.current_tag.pop()
-        return
+        if self.tagstack.pop():
+            self.output_stream.append(Tag(tag, False))
 
     def handle_data(self, data: str) -> None:
-        ctag = self.current_tag[-1]
-        if len(ctag.children) and isinstance(ctag.children[-1], str):
-            ctag.children[-1] += data.replace("\n", "")
-        else:
-            ctag.children.append(data.replace("\n", ""))
-
-    def recurse_tree(self):
-        self._recurse(self.root, 0)
-
-    def _recurse(self, node: Node, indent_level):
-        space = "  " * indent_level
-        space_txt = "  " * (indent_level + 1)
-        if node.isTextNode():
-            print(space_txt + "".join(node.children))
-            return
-
-        print(space + "<" + node.tag + ">")
-        for child in node.children:
-            if isinstance(child, str):
-                print(space_txt + child)
-            else:
-                self._recurse(child, indent_level + 1)
-        print(space + "</" + node.tag + ">")
+        processed_data = preprocess_html_content(data)
+        if len(self.output_stream) and isinstance(self.output_stream[-1], str):
+            self.output_stream[-1] += processed_data
+        elif processed_data:
+            self.output_stream.append(processed_data)
 
 def replace(start: str, init: list[str], repl: list[str]) -> str:
     n = start
     for l, r in zip(init, repl):
-        n = start.replace(l, r)
+        n = n.replace(l, r)
     return n
 
+
 def preprocess_html_content(content: str) -> str:
-    csub = replace(content, ['"', "&", "'", "<", ">"], ["&quot;", "&amp;", "&apos;", "&lt;", "&gt;"])
-    return csub
+    csub = replace(content, ["\"", r"&", r"'", r"<", r">"], ["&quot;", "&amp;", "&apos;", "&lt;", "&gt;"])
+    res = re.sub(r'^\s+$', r'', csub)
+    return res
 
 def main(text = None):
-    session = boto3.Session()
-    client = session.client("polly")
+    # session = boto3.Session()
+    # client = session.client("polly")
 
     # Load the data 
     if not text:
@@ -92,13 +82,12 @@ def main(text = None):
 
     # Parse the json 
     data = json.loads(text)
-    with open("test.html", "w") as f:
-        f.write(data["content"])
+    # with open("test.html", "w") as f:
+    #     f.write(data["content"])
     
     parser = Parser()
     parser.feed(data["content"])
-    rval = parser.recurse_tree()
-    pprint(rval)
+    print("\n".join(str(i) for i in parser.output_stream))
 
     # res = client.synthesize_speech(Text="Hello, world!", OutputFormat="mp3", VoiceId="Joanna")
     # with closing(res["AudioStream"]) as stream, open("out.mp3", "wb") as f:
@@ -107,8 +96,8 @@ def main(text = None):
     
 if __name__ == "__main__":
     data = None
-    if len(sys.argv) > 1:
-        with open("bootstrap_py", "r") as f:
-            data = f.read()
+    # if len(sys.argv) > 1:
+    #     with open("bootstrap_py", "r") as f:
+    #         data = f.read()
 
     main(data)
