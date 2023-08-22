7327fef09d515330fb43a8d1aa3b609a199367ec
Ellis Lunnon <ellislunnon@gmail.com>
2023-08-22 10:11:09 +0100

    Added a script to insert a header

---
+++ readability/header.py
@@ -0,0 +1,14 @@
+#! ./readability/venv/bin/python
+
+import sys
+
+if __name__ != "__main__":
+    sys.exit(1)
+
+data = sys.stdin.buffer.read()
+l = len(data)
+hdr = bytearray([(l >> i * 8) & 0xFF for i in [3, 2, 1, 0]])
+# print(hdr)
+sys.stdout.buffer.write(hdr)
+sys.stdout.buffer.write(data)
+sys.stdout.buffer.flush()
