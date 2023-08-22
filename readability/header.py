73316008b60564ef9a0775e4599af373892c36d2
Ellis Lunnon <ellislunnon@gmail.com>
2023-08-22 10:15:42 +0100

    Remove stuff

--- readability/header.py
+++ readability/header.py
@@ -8,7 +8,6 @@ if __name__ != "__main__":
 data = sys.stdin.buffer.read()
 l = len(data)
 hdr = bytearray([(l >> i * 8) & 0xFF for i in [3, 2, 1, 0]])
-# print(hdr)
 sys.stdout.buffer.write(hdr)
 sys.stdout.buffer.write(data)
 sys.stdout.buffer.flush()
