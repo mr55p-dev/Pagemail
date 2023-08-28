f30bf7147d03a63a3cdceb3df1571ceeb9736c44
Ellis Lunnon <ellislunnon@gmail.com>
2023-08-28 01:39:07 +0100

    Disabled actual readability

--- readability/main.py
+++ readability/main.py
@@ -121,18 +121,40 @@ def main():
 
     try:
         # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/polly/client/start_speech_synthesis_task.html#
-        response = client.start_speech_synthesis_task(
-            Engine='standard',
-            LanguageCode='en-GB',
-            OutputFormat='mp3',
-            OutputS3BucketName='pagemail-speechsynthesis',
-            OutputS3KeyPrefix='test2',
-            Text=inp,
-            TextType='ssml',
-            VoiceId='Amy',
-        )
-        sys.stdout.write(json.dumps(response["SynthesisTask"], default=str))
+        # response = client.start_speech_synthesis_task(
+        #     Engine='standard',
+        #     LanguageCode='en-GB',
+        #     OutputFormat='mp3',
+        #     OutputS3BucketName='pagemail-speechsynthesis',
+        #     OutputS3KeyPrefix='test2',
+        #     Text=inp,
+        #     TextType='ssml',
+        #     VoiceId='Amy',
+        # )
+        # sys.stdout.write(json.dumps(response["SynthesisTask"], default=str))
+        # sys.stdout.flush()
+        sys.stdout.write("""{
+    "ResponseMetadata": {
+        "RequestId": "6b17d1a6-def2-4f22-9d64-0e71101f8c13",
+        "HTTPStatusCode": 200,
+        "HTTPHeaders": { "x-amzn-requestid": "6b17d1a6-def2-4f22-9d64-0e71101f8c13", "content-type": "application/json", "content-length": "472", "date": "Thu, 27 Jul 2023 23:48:54 GMT" },
+        "RetryAttempts": 0
+    },
+    "SynthesisTask": {
+        "Engine": "standard",
+        "TaskId": "2ce9ece0-128f-41bb-8d40-7e723f405d5b",
+        "TaskStatus": "scheduled",
+        "OutputUri": "https://s3.eu-west-2.amazonaws.com/pagemail-speechsynthesis/test2.2ce9ece0-128f-41bb-8d40-7e723f405d5b.mp3",
+        "CreationTime": "2023-07-28 00:48:54.636000+01:00",
+        "RequestCharacters": 6501,
+        "OutputFormat": "mp3",
+        "TextType": "ssml",
+        "VoiceId": "Amy",
+        "LanguageCode": "en-GB"
+    }
+}""")
         sys.stdout.flush()
+
     except Exception as e:
         logging.exception(e)
         logging.fatal("Failed to create new speech synthesis task")
