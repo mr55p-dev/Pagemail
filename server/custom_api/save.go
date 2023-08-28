23cb36fa6a5a84f51e1f6ab154d6702458a6685c
Ellis Lunnon <ellislunnon@gmail.com>
2023-08-28 01:01:03 +0100

    Added new readability to shortcut save

--- server/custom_api/save.go
+++ server/custom_api/save.go
@@ -2,28 +2,30 @@ package custom_api
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase/core"
+
 	"github.com/pocketbase/pocketbase/forms"
 	"github.com/pocketbase/pocketbase/models"
 
 	"github.com/pocketbase/pocketbase"
 )
 
-func saveRecord(app *pocketbase.PocketBase, user_id string, url string) error {
+func saveRecord(app *pocketbase.PocketBase, user_id string, url string) (*models.Record, error) {
 	collection, err := app.Dao().FindCollectionByNameOrId("pages")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	record := models.NewRecord(collection)
-	form := forms.NewRecordUpsert(app, record)
-	form.LoadData(map[string]any{
+	record.Load(map[string]any{
 		"url":     url,
 		"user_id": user_id,
 	})
-	return form.Submit()
+	return record, nil
 }
 
 func SaveRoute(app *pocketbase.PocketBase) echo.HandlerFunc {
@@ -43,9 +45,33 @@ func SaveRoute(app *pocketbase.PocketBase) echo.HandlerFunc {
 		if user_id == "" {
 			return c.String(http.StatusBadRequest, "Could not retrieve user id")
 		}
-		if err := saveRecord(app, user_id, url); err != nil {
+		record, err := saveRecord(app, user_id, url)
+		if err != nil {
 			return c.String(http.StatusBadRequest, fmt.Sprintf("Failed to store this record: %s", err))
 		}
+
+		log.Printf("Before-- url: %s, title: %s", record.GetString("url"), record.GetString("title"))
+		collection, err := app.Dao().FindCollectionByNameOrId("pages")
+		if err != nil {
+			log.Panic(err)
+		}
+
+		form := forms.NewRecordUpsert(app, record)
+		err = form.Submit()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "failed to save record")
+		}
+
+		go func() {
+			err = app.OnRecordAfterCreateRequest("pages").Trigger(&core.RecordCreateEvent{
+				HttpContext: c,
+				Record:      record,
+				BaseCollectionEvent: core.BaseCollectionEvent{
+					Collection: collection,
+				},
+			})
+		}()
+
 		return c.String(http.StatusOK, "Saved")
 	}
 }
