7cd1ac9977c6a089ef5ff78c523290b0f11d5f59
Ellis Lunnon <ellislunnon@gmail.com>
2023-11-20 22:38:23 +0000

    Fix: add 1 to month number

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,5 +1,5 @@
 export function dateToString(date: Date) {
-  return `${date.getDate().toString().padStart(2, "0")}-${
-    date.getMonth().toString().padStart(2, "0") + 1
-  }-${date.getFullYear().toString().padStart(2, "0")}`;
+  return `${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getFullYear().toString().padStart(2, "0")}`;
 }
