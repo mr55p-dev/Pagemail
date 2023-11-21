419981113409ce423e4c1e4c00f9c36694da374f
Ellis Lunnon <ellislunnon@gmail.com>
2023-11-21 16:39:17 +0000

    Some cleanup

--- readability/main.py
+++ readability/main.py
@@ -1,5 +1,4 @@
 from parser import Parser, Dispatcher
-from unittest.mock import Mock
 
 import sys
 import traceback
@@ -27,10 +26,7 @@ def run(session, input: str):
 
 def main():
     input = sys.stdin.read()
-    if len(sys.argv) > 1 and sys.argv[1] == "--test":
-        session = Mock()
-    else:
-        session = boto3.Session()
+    session = boto3.Session()
 
     run(session, input)
 
