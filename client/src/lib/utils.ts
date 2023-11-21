664ab0accf6c960d7999034c94832412160170a3
Ellis Lunnon <67969496+mr55p-dev@users.noreply.github.com>
2023-11-21 06:37:17 +0000

    fix: Display dates as DD-MM-YYYY (#40)

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,5 @@
+export function dateToString(date: Date) {
+  return `${date.getDate().toString().padStart(2, "0")}-${(date.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${date.getFullYear().toString().padStart(2, "0")}`;
+}
