a43a6a2399ab649da5b6853b6ed2ad418e810359
Ellis Lunnon <ellislunnon@gmail.com>
2024-01-10 13:59:08 +0000

    removed old files

--- client/src/lib/const.ts
+++
@@ -1,1 +0,0 @@
-export const iosShortcutLink = "https://www.icloud.com/shortcuts/4a3b7f40f0c94a318df2af0a8d6ae917"
--- server/models/aws.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type S3Config struct {
-	ReadabilityBucket string
-}
