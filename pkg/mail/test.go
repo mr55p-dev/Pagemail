7700d1e87d1c23862c78591626566ddae0e815f9
Ellis Lunnon <67969496+mr55p-dev@users.noreply.github.com>
2024-02-17 07:02:39 +0530

    feat: added cli tool (#56)
    fixed mail sending

--- pkg/mail/test.go
+++ pkg/mail/test.go
@@ -2,13 +2,11 @@ package mail
 
 import (
 	"context"
-
-	"github.com/mr55p-dev/pagemail/pkg/db"
 )
 
 type TestClient struct{}
 
-func (*TestClient) SendMail(ctx context.Context, user *db.User, body string) error {
-	log.Info("Test sending mail", "user", user.Email, "body", body)
+func (*TestClient) SendMail(ctx context.Context, user *User, body string) error {
+	log.Info("Test sending mail", "user", user.Email)
 	return nil
 }
