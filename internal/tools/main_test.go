106f627f6b65d3f040f08763d56a600b9872b9ef
Ellis Lunnon <ellislunnon@gmail.com>
2024-05-13 09:14:18 +0100

    feat: use base32 encoded strings for IDs

--- internal/tools/main_test.go
+++ internal/tools/main_test.go
@@ -1,15 +1,25 @@
 package tools
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGenerateId(t *testing.T) {
-	vals := []int{1, 5, 100}
+	assert := assert.New(t)
+	vals := []int{1, 5, 8, 20, 25, 50, 75, 100}
+	outs := make([]string, 0, len(vals))
 	for _, v := range vals {
 		id := GenerateNewId(v)
-		if len(id) != v {
-			t.Log("Requested", v, "got", id, len(id))
-			t.FailNow()
+		outs = append(outs, id)
+		assert.Len(id, v)
+		t.Log(id)
+	}
+
+	for idx, id := range outs {
+		for j := idx + 1; j < len(outs); j++ {
+			assert.NotEmpty(id, outs[j], "Collision in ids")
 		}
-		t.Log("Requested", v, "chars:", id)
 	}
 }
