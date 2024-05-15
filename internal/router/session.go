940eb916099e9ff593a72f4e3f4ce38768a15695
Ellis Lunnon <ellis.lunnon@sainsburys.co.uk>
2024-05-15 10:37:47 +0100

    wip: added a decode test to the key

--- internal/router/session.go
+++ internal/router/session.go
@@ -1,23 +1,28 @@
 package router
 
 import (
-	"errors"
+	"bytes"
+	"encoding/base64"
 	"io"
 
 	"github.com/gorilla/sessions"
 )
 
-const NumKeyBytes = 16
-
 func loadCookieKey(router *Router, input io.Reader) error {
-	key := make([]byte, NumKeyBytes)
-	n, err := input.Read(key)
+	key, err := io.ReadAll(input)
 	if err != nil {
 		return err
 	}
-	if n < NumKeyBytes {
-		return errors.New("Cookie key source has insufficient bytes")
+
+	// TEMP
+	// try to base64 decode the key
+	// if it fails, it's not base64 encoded
+	dec := base64.NewDecoder(base64.StdEncoding, bytes.NewReader(key))
+	_, err = io.ReadAll(dec)
+	if err != nil {
+		panic(err)
 	}
+
 	router.Sessions = sessions.NewCookieStore(key)
 	return nil
 }
