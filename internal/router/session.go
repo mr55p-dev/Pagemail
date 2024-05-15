be7041c45f9b9bf00766a25c720fd7afa9a238ce
Ellis Lunnon <ellis.lunnon@sainsburys.co.uk>
2024-05-15 11:19:21 +0100

    fix: use actual key data

--- internal/router/session.go
+++ internal/router/session.go
@@ -1,11 +1,8 @@
 package router
 
 import (
-	"bytes"
-	"encoding/base64"
 	"io"
 
-	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 )
 
@@ -15,14 +12,6 @@ func loadCookieKey(router *Router, input io.Reader) error {
 		return err
 	}
 
-	// TEMP
-	dec := base64.NewDecoder(base64.StdEncoding, bytes.NewReader(key))
-	_, err = io.ReadAll(dec)
-	if err != nil {
-		panic(err)
-	}
-
-	_ = key
-	router.Sessions = sessions.NewCookieStore(securecookie.GenerateRandomKey(20))
+	router.Sessions = sessions.NewCookieStore(key)
 	return nil
 }
