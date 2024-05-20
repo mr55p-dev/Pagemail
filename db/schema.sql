84a20cd1da8b471b98e8149e9289e6e693aece9a
Ellis Lunnon <ellislunnon@gmail.com>
2024-05-20 16:12:26 +0100

    wip: written mega migration which actually worked

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS schema_migrations (version varchar(128) primary key);
-CREATE TABLE IF NOT EXISTS pages (
+CREATE TABLE IF NOT EXISTS "schema_migrations" (version varchar(128) primary key);
+CREATE TABLE IF NOT EXISTS "pages" (
 	id TEXT PRIMARY KEY NOT NULL,
 	user_id TEXT NOT NULL,
 	url TEXT NOT NULL,
@@ -14,22 +14,33 @@ CREATE TABLE IF NOT EXISTS pages (
 
 	FOREIGN KEY(user_id) REFERENCES users(id)
 );
-CREATE TABLE IF NOT EXISTS users (
-	id TEXT UNIQUE NOT NULL PRIMARY KEY,
-	username TEXT UNIQUE NOT NULL,
-	email TEXT UNIQUE NOT NULL ,
-	password BLOB NOT NULL,
-	reset_token BLOB,
-	reset_token_exp DATETIME,
-	avatar TEXT,
-	subscribed BOOL NOT NULL DEFAULT false,
-	shortcut_token BLOB NOT NULL,
-	has_readability BOOL NOT NULL DEFAULT false,
-	created DATETIME NOT NULL,
-	updated DATETIME NOT NULL
+CREATE TABLE IF NOT EXISTS "users" (
+    id TEXT PRIMARY KEY NOT NULL,
+    email TEXT NOT NULL,
+    username TEXT NOT NULL,
+	subscribed BOOL NOT NULL DEFAULT FALSE,
+    has_readability BOOL NOT NULL DEFAULT FALSE,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE auth (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT,
+    platform TEXT NOT NULL,
+    password_hash BLOB,
+    password_reset_token BLOB,
+    password_reset_expiry DATETIME,
+    credential BLOB,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES "users" (id) ON DELETE CASCADE,
+
+    UNIQUE (user_id, platform),
+    UNIQUE (platform, credential)
 );
 -- Dbmate schema migrations
-INSERT INTO schema_migrations (version) VALUES
+INSERT INTO "schema_migrations" (version) VALUES
   ('20240104200335'),
   ('20240105072653'),
   ('20240105122600'),
@@ -38,4 +49,5 @@ INSERT INTO schema_migrations (version) VALUES
   ('20240513090548'),
   ('20240515151753'),
   ('20240520062158'),
-  ('20240520065956');
+  ('20240520065956'),
+  ('20240520143026');
