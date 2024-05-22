de1530e1de4b425fda209338afc24b587f3743c5
Ellis <ellislunnon@gmail.com>
2024-05-22 15:47:49 +0100

    feat: added ECR and cloudwatch sources for readability

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -29,3 +29,35 @@ resource "aws_ecr_lifecycle_policy" "pagemail" {
 }
 EOF
 }
+
+resource "aws_ecr_repository" "pagemail_readability" {
+  name                 = "pagemail-readability"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "pagemail_readability" {
+  repository = aws_ecr_repository.pagemail_readability.name
+  policy     = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "cleanup untagged images",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 1
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
