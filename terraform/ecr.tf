500f3670f7222c77d3beaffa3fd241094292ac6e
Ellis <ellislunnon@gmail.com>
2024-05-22 15:27:47 +0100

    refactor: removed unused ECR

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -29,36 +29,3 @@ resource "aws_ecr_lifecycle_policy" "pagemail" {
 }
 EOF
 }
-
-
-resource "aws_ecr_repository" "pagemail_migrations" {
-  name                 = "pagemail-migrations"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "pagemail_migrations" {
-  repository = aws_ecr_repository.pagemail_migrations.name
-  policy     = <<EOF
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "cleanup untagged images",
-      "selection": {
-        "tagStatus": "untagged",
-        "countType": "sinceImagePushed",
-        "countUnit": "days",
-        "countNumber": 1
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
-}
-EOF
-}
