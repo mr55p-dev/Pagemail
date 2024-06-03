baad2a374e23a2c6fb5e15e38a9b8fd6335c72f6
Ellis Lunnon <ellislunnon@gmail.com>
2024-06-03 20:14:18 +0100

    feat: finished migration, added the new pages!

---
+++ db/migrations/20240602181219_articles-and-readings.sql
@@ -0,0 +1,36 @@
+-- migrate:up
+CREATE TABLE IF NOT EXISTS articles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    page_id TEXT NOT NULL,
+    state TEXT NOT NULL DEFAULT 'unknown',
+    reason TEXT,
+    html BLOB NOT NULL,
+    content BLOB NOT NULL,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (page_id),
+    FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS readings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    article_id TEXT NOT NULL,
+    job_id TEXT,
+    state TEXT NOT NULL DEFAULT 'unknown',
+    reason TEXT,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (article_id),
+    FOREIGN KEY (id) REFERENCES articles (id) ON DELETE CASCADE
+);
+
+ALTER TABLE pages DROP COLUMN reading_job_status;
+ALTER TABLE pages DROP COLUMN reading_job_id;
+
+-- migrate:down
+ALTER TABLE pages ADD COLUMN reading_job_id TEXT;
+ALTER TABLE pages ADD COLUMN reading_job_status TEXT NOT NULL DEFAULT 'unknown';
+DROP TABLE readings;
+DROP TABLE articles;
--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,23 @@
-CREATE TABLE IF NOT EXISTS schema_migrations (version varchar(128) primary key);
+CREATE TABLE IF NOT EXISTS schema_migrations (version varchar(128) PRIMARY KEY);
 CREATE TABLE IF NOT EXISTS users (
-    id TEXT PRIMARY KEY NOT NULL,
-    email TEXT NOT NULL,
-    username TEXT NOT NULL,
-	subscribed BOOL NOT NULL DEFAULT FALSE,
-    has_readability BOOL NOT NULL DEFAULT FALSE,
-    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id text PRIMARY KEY NOT NULL,
+    email text NOT NULL,
+    username text NOT NULL,
+    subscribed bool NOT NULL DEFAULT FALSE,
+    has_readability bool NOT NULL DEFAULT FALSE,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE auth (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id TEXT NOT NULL,
-    platform TEXT NOT NULL,
-    password_hash BLOB,
-    password_reset_token BLOB,
-    password_reset_expiry DATETIME,
-    credential BLOB,
-    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id integer PRIMARY KEY AUTOINCREMENT,
+    user_id text NOT NULL,
+    platform text NOT NULL,
+    password_hash blob,
+    password_reset_token blob,
+    password_reset_expiry datetime,
+    credential blob,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
 
@@ -25,29 +25,56 @@ CREATE TABLE auth (
     UNIQUE (platform, credential)
 );
 CREATE TABLE IF NOT EXISTS pages (
-    id TEXT PRIMARY KEY,
-    user_id TEXT NOT NULL,
-    url TEXT NOT NULL,
-    title TEXT,
-    description TEXT,
-    image_url TEXT,
-    preview_state TEXT DEFAULT 'unknown' NOT NULL,
-    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, readable BOOL NOT NULL DEFAULT false, reading_job_status STRING NOT NULL DEFAULT 'unknown', reading_job_id STRING,
+    id text PRIMARY KEY,
+    user_id text NOT NULL,
+    url text NOT NULL,
+    title text,
+    description text,
+    image_url text,
+    preview_state text DEFAULT 'unknown' NOT NULL,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    readable bool NOT NULL DEFAULT FALSE,
 
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+CREATE TABLE articles (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    page_id text NOT NULL,
+    state text NOT NULL DEFAULT 'unknown',
+    reason text,
+    html blob NOT NULL,
+    content blob NOT NULL,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (page_id),
+    FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
+);
+CREATE TABLE readings (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    article_id text NOT NULL,
+    job_id text,
+    state text NOT NULL DEFAULT 'unknown',
+    reason text,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    UNIQUE (article_id),
+    FOREIGN KEY (id) REFERENCES articles (id) ON DELETE CASCADE
+);
 -- Dbmate schema migrations
 INSERT INTO schema_migrations (version) VALUES
-  ('20240104200335'),
-  ('20240105072653'),
-  ('20240105122600'),
-  ('20240510132816'),
-  ('20240510134137'),
-  ('20240513090548'),
-  ('20240515151753'),
-  ('20240520062158'),
-  ('20240520065956'),
-  ('20240520143026'),
-  ('20240520185930'),
-  ('20240522124057');
+('20240104200335'),
+('20240105072653'),
+('20240105122600'),
+('20240510132816'),
+('20240510134137'),
+('20240513090548'),
+('20240515151753'),
+('20240520062158'),
+('20240520065956'),
+('20240520143026'),
+('20240520185930'),
+('20240522124057'),
+('20240602181219');
